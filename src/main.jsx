83a0833f5a8fe954809aadf2edf4da058d3710dc
Ryan M. Smith <47118851+pecosryan@users.noreply.github.com>
2026-02-04 15:24:10 -0600

    Initialize main.jsx with React and Intelligence component

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Intelligence from './Intelligence'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <React.StrictMode>
+        <Intelligence />
+    </React.StrictMode>React.StrictMode>,
+  )</React.StrictMode>
